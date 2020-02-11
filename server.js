c2e5c5322c870b4823509fb39269da2e28b08933
Green Vine <barryaervin@gmail.com>
2020-02-11 16:02:26 -0500

    adde cronjob to increment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+var CronJob = require('cron').CronJob;
 
 const app = express()
 
 const port = process.env.PORT || 3000
-const muleNumber = [5];
+const muleNumber = [210];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,7 +14,7 @@ app.get('/smiirl/muleNumber', function (req, res) {
 })
 
 app.get('/smiirl/update/:number', function (req, res) {
-    muleNumber[0] = req.params.number
+    muleNumber[0] += req.params.number
     res.json('Updated Moscow Mule total to ' + muleNumber[0])
 })
 
@@ -21,5 +22,9 @@ app.listen(port, function () {
     console.log('App listening on PORT: ' + port)
 })
 
+var job = new CronJob("0 19,20,21,22,23,0,1,2 * * *", function () {
+    muleNumber[0] += 1
+}, null, true, 'America/Los_Angeles');
+job.start();
 // Use Imap to search my email per da
 
