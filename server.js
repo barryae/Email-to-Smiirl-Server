1b99f3c3f9f1d4794e3b5a50ae05be3abe9f765a
Green Vine <barryaervin@gmail.com>
2020-02-11 22:35:49 -0500

    yes

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-var CronJob = require('cron').CronJob;
+const CronJob = require('cron').CronJob;
 
 const app = express()
 
 const port = process.env.PORT || 3000
-const muleNumber = [210];
+const muleNumber = 210;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/smiirl/muleNumber', function (req, res) {
-    res.json({ number: muleNumber[0] }); //
+    res.json({ number: muleNumber[0] });
 })
 
 app.get('/smiirl/update/:number', function (req, res) {
-    muleNumber[0] += req.params.number
+    muleNumber[0] = req.params.number
     res.json('Updated Moscow Mule total to ' + muleNumber[0])
 })
 
@@ -22,9 +22,8 @@ app.listen(port, function () {
     console.log('App listening on PORT: ' + port)
 })
 
-var job = new CronJob("0 19,20,21,22,23,0,1,2 * * *", function () {
+const job = new CronJob("0 19,20,21,22,23,0,1,2 * * *", function () {
     muleNumber[0] += 1
 }, null, true, 'America/Los_Angeles');
 job.start();
-// Use Imap to search my email per da
 
