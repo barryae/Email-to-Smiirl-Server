0c63ec40c73d9bd2580adced151018b80d6eb5ea
Green Vine <barryaervin@gmail.com>
2020-02-18 15:27:43 -0500

    increment

--- increment.js
+++ increment.js
@@ -1,16 +1,19 @@
 const MuleNumber = require('./model')
 
-MuleNumber.findOne({ title: "MuleNumber" })
-    .then(result => {
-        MuleNumber.updateOne({ title: "MuleNumber" }, { number: (result.number + 1) })
-            .then(result => {
-                console.log(result)
-                res.json(result)
-            })
-            .catch(err => {
-                res.status(422).json(err)
-            })
-    })
-    .catch(err => {
-        res.status(422).json(err)
-    })
+function increment() {
+    MuleNumber.findOne({ title: "MuleNumber" })
+        .then(result => {
+            MuleNumber.updateOne({ title: "MuleNumber" }, { number: (result.number + 1) })
+                .then(result => {
+                    console.log(result)
+                    res.json(result)
+                })
+                .catch(err => {
+                    res.status(422).json(err)
+                })
+        })
+        .catch(err => {
+            res.status(422).json(err)
+        })
+}
+increment();
