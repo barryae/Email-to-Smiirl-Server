9fc2da824df3af8f3e8473866c3f19d222c10f64
Green Vine <barryaervin@gmail.com>
2020-02-18 15:22:49 -0500

    increment

--- increment.js
+++ increment.js
@@ -4,6 +4,7 @@ MuleNumber.findOne({ title: "MuleNumber" })
     .then(result => {
         MuleNumber.updateOne({ title: "MuleNumber" }, { number: (result.number + 1) })
             .then(result => {
+                console.log(result)
                 res.json(result)
             })
             .catch(err => {
